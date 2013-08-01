d1f9ce69371593f8d2e80621dfe4417c1a63ba95
Jakub Jarosz <jakub.s.jarosz@gmail.com>
2013-08-01 14:05:37 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='nc',
+      version='0.0.1',
+      description='csv to nc converter',
+      author='Jakub Jarosz',
+      author_email='jakub.s.jarosz at gmail.com',
+      url='https://github.com/qba73/nc',
+      py_modules=['nc'])
